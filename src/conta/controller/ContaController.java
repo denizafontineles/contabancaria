d84b91089a7c63e0bfb00234ad601587c3ef9e7d
denizafontineles <denizafontineles@outlook.com>
2024-03-09 10:49:28 -0300

    adiciona atividade - métodos CRUD - pt 01

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -1,9 +1,14 @@
 package conta.controller;
 
+import java.util.ArrayList;
+
 import conta.model.Conta;
 import conta.repository.ContaRepository;
 
 public class ContaController implements ContaRepository{
+	
+	private ArrayList<Conta> listaContas = new ArrayList <Conta>();
+	int numero = 0;
 
 	@Override
 	public void procurarPorNumero(int numero) {
@@ -13,13 +18,15 @@ public class ContaController implements ContaRepository{
 
 	@Override
 	public void listarTodas() {
-		// TODO Auto-generated method stub
-		
+		for (var conta: listaContas) {
+			conta.visualizar();
+		}
 	}
 
 	@Override
 	public void cadastrar(Conta conta) {
-		// TODO Auto-generated method stub
+		listaContas.add(conta);
+		System.out.println("\nA Conta número: " + conta.getNumero() + " foi criada com sucesso!");
 		
 	}
 
@@ -44,13 +51,14 @@ public class ContaController implements ContaRepository{
 	@Override
 	public void depositar(int numero, float valor) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 	
+	public int gerarNumero() {
+		return ++ numero;
+	}
 }
